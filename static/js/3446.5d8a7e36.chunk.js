58d81dda55254e93e31bfdffd51e51b27f163d92
abcde-rgb <abcde-rgb@users.noreply.github.com>
2026-08-01 14:30:35 +0000

    deploy: 55d88deca0ef5be2c22631776c933b8b2bb3828b

---
+++ static/js/3446.5d8a7e36.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[3446],{63446(e,n,s){s.d(n,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=3446.5d8a7e36.chunk.js.map
---
+++ static/js/4203.95bde386.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[4203],{94203(e,n,s){s.d(n,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=4203.95bde386.chunk.js.map
